61a43b80cfa9d2bf9be11985206cd1976bab7c83
ravipatelctf <ravipatelctf@gmail.com>
2025-07-28 11:33:22 +0530

    done: BE1.1_HW2

--- BE1.1_CW/models/car.models.js
+++ BE1.1_CW/models/car.models.js
@@ -1,10 +1,47 @@
 const mongoose = require("mongoose");
 
 const carSchema = new mongoose.Schema({
-    model: String,
-    releaseYear: Number,
-    make: String,
-});
+    make: {
+        type: String,
+        required: true,
+    },
+    model: {
+        type: String,
+        required: true,
+    },
+    year: Number,
+    mileage: Number,
+    fuelType: {
+        type: String,
+        enum: ["Gasoline", "Diesel", "Electric", "Hybrid"],
+    },
+    transmission: {
+        type: String,
+        enum: ["Automatic", "Manual"],
+    },
+    bodyStyle: String,
+    color: String,
+    isCertifiedPreowned: {
+        type: Boolean,
+        default: false,
+    },
+    isFourWheelDrive: {
+        type: Boolean,
+        default: false,
+    },
+    isLuxury: {
+        type: Boolean,
+        default: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+},
+{
+    timestamps: true,
+},
+);
 
 const Car = mongoose.model("Car", carSchema);
 
