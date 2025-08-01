19b87a59aa6a61d8d49c199f165441a3eceb7e07
ravipatelctf <ravipatelctf@gmail.com>
2025-08-01 15:51:07 +0530

    update

--- BE3/BE3.3_HW2/index.js
+++ BE3/BE3.3_HW2/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json())
+
 /*
 1. Write a GET route "/" which sends a message "Hello, Express server.". Test your API with Postman.
 
@@ -33,6 +34,46 @@ app.delete("/movies/:id", (req, res) => {
         movies.splice(index, 1);
         res.status(200).json
     }
+});
+
+/*
+3. Write a GET route "/movies" which sends the movies array in response. Test your API with Postman.
+
+*/
+app.get("/movies", (req, res) => {
+    res.send(movies);
+});
+
+/*
+4. Write a DELETE route which deletes an item with id 2, from the pre-defined items array. Send an error message, "Item not found." in case any item is not found.
+
+Pre-defined items array:
+
+*/
+const items = [
+    { id: 1, itemName: 'Spoon', color: 'Silver', quantity: 8},
+    { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 },
+    { id: 3, itemName: 'Plate', color: 'Off-White', quantity: 6 }
+];
+
+app.delete("/items/:id", (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const index = items.findIndex(item => item.id === itemId);
+
+    if(index === -1) {
+        res.status(400).json({error: "Item Not Found!"});
+    } else {
+        items.splice(index, 1);
+        res.status(200).json({message: "Item deleted successfully."});
+    }
+});
+
+/*
+5. Write a GET route "/items" which sends the items array in response. Test your API with Postman and check that the above item got deleted.
+
+*/
+app.get("/items", (req, res) => {
+    res.send(items);
 });
 
 
