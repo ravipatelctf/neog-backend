9ed38970f54400b3f28606a53f1551c444680010
ravipatelctf <ravipatelctf@gmail.com>
2025-08-04 20:57:22 +0530

    update

--- BE4/BE4.4_HW1/index.js
+++ BE4/BE4.4_HW1/index.js
@@ -1,51 +1,47 @@
 const {initializeDatabase} = require("./db/db.config");
 const Restaurant = require("./models/restaurant.models");
 
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
 initializeDatabase();
 
 /*
-1. Create a function that accepts a restaurant ID and an object with updated data, and updates the restaurant with the provided ID. Take the _id of the restaurant which has the name Yo China and update its rating from 3.9 to 4.1. Console the updated restaurant.
+1. Create an API to update a restaurant data by their ID in the Database. Update the cuisine of an existing restaurant. Test your API with Postman.
+
 */
 
-const updateRestaurantRating = async (restaurantId, dataToUpdate) => {
+const updateRestaurantById = async (restaurantId, dataToUpdate) => {
     try {
         const updatedRestaurant = await Restaurant.findByIdAndUpdate(restaurantId, dataToUpdate, {new: true});
-        console.log(updatedRestaurant);
+        return updatedRestaurant;
     } catch (error) {
         throw error;
     }
 };
 
-updateRestaurantRating("6888b6ff2df6c12fab93d115", {rating: 4.1});
-
-
-/*
-2. Create a function that accepts a restaurant name and an object with updated data, and updates the restaurant. Take the restaurant which has the name "Somi" and update its name from "Somi" to "Som Sarovar". Console the updated restaurant.
-*/
-
-const updateRestaurantName = async (restaurantName, dataToUpdate) => {
+app.post("/restaurants/:restaurantId", async (req, res) => {
     try {
-        const updatedRestaurant = await Restaurant.findOneAndUpdate({name: restaurantName}, dataToUpdate, {new: true});
-        console.log(updatedRestaurant);
+        const updatedRestaurant = await updateRestaurantById(req.params.restaurantId, req.body);
+        if(updatedRestaurant) {
+            res
+                .status(200)
+                .json({message: "Restaurant updated successfully.", updatedRestaurant: updatedRestaurant});
+        } else {
+            res
+                .status(404)
+                .json({error: "Restaurant Not Found!"});
+        }
     } catch (error) {
-        throw error;
+        res
+            .status(500)
+            .json({error: "Failed to update restaurant!"});
     }
-}
+});
 
-updateRestaurantName("Somi", {name: "Som Sarovar"});
-
-
-/*
-3. Create a function that accepts a restaurant's phone number and an object with updated data, and updates the restaurant. Take the restaurant which has the phone number "+1288997392" and update isDeliveryAvailable option to true. Console the updated restaurant.
-*/
-
-const updateRestaurantDelivery = async (phoneNumber, dataToUpdate) => {
-    try {
-        const updatedRestaurant = await Restaurant.findOneAndUpdate({phoneNumber}, dataToUpdate, {new: true});
-        console.log(updatedRestaurant);
-    } catch (error) {
-        throw error;
-    }
-};
-
-updateRestaurantDelivery("+1288997392", {isDeliveryAvailable: true});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
