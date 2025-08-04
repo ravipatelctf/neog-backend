af1e6d0fd87b10c54399fa6433115407308fbd5e
ravipatelctf <ravipatelctf@gmail.com>
2025-08-04 18:28:44 +0530

    update

--- BE4/BE4.3_HW2/index.js
+++ BE4/BE4.3_HW2/index.js
@@ -1,36 +1,44 @@
 const {initializeDatabase} = require("./db/db.connect");
 const Hotel = require("./models/hotel.models");
 
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
 initializeDatabase();
 
 /*
-1. Create a function deleteHotelById that accepts a hotel ID and deletes the hotel data from the db. Take any hotel id from your database and delete the records of that hotel.
+1. Create an API with route "/hotels/:hotelId" to delete a hotel data by their ID in the Database. Test your API with Postman.
+
 */
 
 async function deleteHotelById(hotelId) {
     try {
         const deletedHotel = await Hotel.findByIdAndDelete(hotelId);
-        console.log("This hotel data was deleted:", deletedHotel);
+        return deletedHotel;
     } catch (error) {
         throw error;
     }
 };
 
-deleteHotelById("68888cb0033864b01b06893c");
-
-
-/*
-2. Create a function deleteHotelByPhoneNumber that accepts a hotel's phone number and deletes the hotel data from the db. Take any hotel phone number from your database and delete the records of that hotel.
-*/
-
-
-async function deleteHotelByPhoneNumber(phoneNumber) {
+app.delete("/hotels/:hotelId", async (req, res) => {
     try {
-        const deletedHotel = await Hotel.findOneAndDelete({phoneNumber});
-        console.log("This hotel data was deleted:", deletedHotel);
+        const deletedHotel = await deleteHotelById(req.params.hotelId);
+        if (deletedHotel) {
+            res
+                .status(200)
+                .json({message: "Hotel deleted successfully."});
+        }
     } catch (error) {
-        throw error;
+        res
+            .status(500)
+            .json({error: "Failed to delete hotel data!"});
     }
-};
+});
 
-deleteHotelByPhoneNumber("+1997687392");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
